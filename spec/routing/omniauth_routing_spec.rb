ebb838293207c9aa6a05db3288810119cc7fe6d1
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-23 04:35:43 +1100

    Refactored omniauth spec support

--- spec/routing/omniauth_routing_spec.rb
+++ spec/routing/omniauth_routing_spec.rb
@@ -2,20 +2,12 @@ require 'support/omniauth'
 
 describe 'routes for user login' do
   describe 'steam' do
-    before do
-      # Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
-    end
+    it 'first time' do
+      pending('Turn into a Capybara test')
 
-    describe 'first login' do
-      before do
-        OmniAuth.config.add_mock(:steam, 'uid' => '12345')
-        Rails.application.env_config['omnauth.auth'] = OmniAuth.config.mock_auth[:steam]
-        p Rails.application.env_config['omniauth.auth']
-      end
+      mock_auth_hash
 
-      pending it 'redirects to callback' do
-        expect(get('/users/auth/steam')).to route_to(controller: 'users/omniauth_callbacks', action: 'steam')
-      end
+      expect(get('/users/auth/steam')).to route_to(controller: 'users/omniauth_callbacks', action: 'steam')
     end
   end
 end
--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,3 +1,11 @@
-OmniAuth.configure do |config|
-  config.test_mode = true
+OmniAuth.config.test_mode = true
+
+RSpec.configure do
+  def mock_auth_hash
+    OmniAuth.config.mock_auth[:steam] = {
+      'provider' => 'steam',
+      'uid' => '12345',
+      'info' => { 'nickname' => 'foobar' },
+    }
+  end
 end
