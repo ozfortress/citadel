685037af9a14c6f344f2b6f06c92078f26877a89
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-26 13:31:46 +1100

    Made name and steam_id un-nullable for users

--- db/migrate/20160121163042_create_users.rb
+++ db/migrate/20160121163042_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string   :name
-      t.integer  :steam_id, limit: 8
+      t.string   :name, null: false
+      t.integer  :steam_id, limit: 8, null: false
 
       # Devise
       ## Rememberable
