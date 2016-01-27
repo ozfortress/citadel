d39284fe550882caf6e238f8443b3379b6816be9
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-28 05:29:08 +1100

    Added tests for meta controller

--- spec/controllers/meta_controller_spec.rb
+++ spec/controllers/meta_controller_spec.rb
@@ -3,18 +3,29 @@ require 'support/devise'
 
 describe MetaController do
   describe 'GET #index' do
-    let(:user) { create(:user) }
-
-    before do
+    it 'succeeds for authorized user' do
+      user = create(:user)
       user.grant(:edit, :games)
-    end
-
-    it 'returns http success' do
       sign_in user
 
       get :index
 
       expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects to root for unauthorized user' do
+      user = create(:user)
+      sign_in user
+
+      get :index
+
+      expect(response).to redirect_to(root_path)
+    end
+
+    it 'redirects to root for unauthenticated' do
+      get :index
+
+      expect(response).to redirect_to(root_path)
     end
   end
 end
