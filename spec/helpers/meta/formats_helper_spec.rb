e16f33a5a038c6971d615450347ba8eee7754bb6
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-28 06:19:12 +1100

    Cleaned up helper tests

--- spec/helpers/meta/formats_helper_spec.rb
+++ spec/helpers/meta/formats_helper_spec.rb
@@ -1,15 +1,4 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the Meta::FormatsHelper. For example:
-#
-# describe Meta::FormatsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe Meta::FormatsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Meta::FormatsHelper do
 end
