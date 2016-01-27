9bb2965204039bd759cec264c4696cc58c747771
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-27 17:02:21 +1100

    Allowed teams to have blank descriptions

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,5 +12,6 @@ describe Team do
   it { should validate_length_of(:name).is_at_least(1) }
   it { should validate_length_of(:name).is_at_most(64) }
 
-  it { should validate_presence_of(:description) }
+  it { should allow_value('').for(:description) }
+  it { should validate_length_of(:description).is_at_least(0) }
 end
