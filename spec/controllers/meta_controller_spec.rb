907ea332a73207b3d11679aa1d2e4089c3b0d19c
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-27 20:38:49 +1100

    Made meta controller work with permission system

--- spec/controllers/meta_controller_spec.rb
+++ spec/controllers/meta_controller_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
+require 'support/devise'
 
-RSpec.describe MetaController, type: :controller do
+describe MetaController do
   describe 'GET #index' do
+    let(:user) { create(:user) }
+
+    before do
+      user.grant(:edit, :games)
+    end
+
     it 'returns http success' do
+      sign_in user
+
       get :index
+
       expect(response).to have_http_status(:success)
     end
   end
