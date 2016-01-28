a83f4c8ec97ebafa1f154beee5d4858e7055b40a
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-01-28 15:24:45 +1100

    Added recruitment to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,8 @@ module ApplicationHelper
     case name
     when :home
       controller_name == 'pages' && action_name == 'home'
+    when :recruitment
+      controller_name == 'users' && action_name == 'index'
     when :teams
       controller_name == 'teams'
     when :leagues
