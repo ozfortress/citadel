6cae2fea4db705b3add0abdca29a73b708b57afe
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-02-08 16:56:40 +1100

    Wrote meta/games spec

--- spec/controllers/meta/games_controller_spec.rb
+++ spec/controllers/meta/games_controller_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 describe Meta::GamesController do
+  let(:admin) { create(:user) }
+
+  before do
+    admin.grant(:edit, :games)
+  end
+
+  describe 'GET #index' do
+    it 'succeeds for authorized user' do
+      sign_in admin
+
+      get :index
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'succeeds for authorized user' do
+      sign_in admin
+
+      get :new
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'succeeds for authorized user' do
+      sign_in admin
+
+      post :create, game: { name: 'Bar' }
+
+      game = Game.first
+      expect(game.name).to eq('Bar')
+    end
+  end
 end
