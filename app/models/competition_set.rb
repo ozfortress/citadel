dce66ddb13ff5e6bd9f441a62947f26b9aa00ec5
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-02-20 16:42:00 +1100

    Added default set values

--- app/models/competition_set.rb
+++ app/models/competition_set.rb
@@ -5,4 +5,13 @@ class CompetitionSet < ActiveRecord::Base
   validates :match,           presence: true
   validates :home_team_score, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :away_team_score, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.home_team_score = 0 unless home_team_score.present?
+    self.away_team_score = 0 unless away_team_score.present?
+  end
 end
