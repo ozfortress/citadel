665a1ec4f2ac035b567a8f22a92baca048fc46ce
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-02-23 18:06:29 +1100

    Added basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with name: 'ozf', password: 'ozf' unless Rails.env.test?
+
   protected
 
   def require_login
