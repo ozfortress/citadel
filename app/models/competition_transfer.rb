0de77e7a20af7c34ea41fa5b6a2752a2f77556c9
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-03-10 13:16:11 +1100

    Fixed missing class_name on CompetitionTransfer rosters

--- app/models/competition_transfer.rb
+++ app/models/competition_transfer.rb
@@ -1,6 +1,6 @@
 class CompetitionTransfer < ActiveRecord::Base
   belongs_to :user
-  belongs_to :roster, foreign_key: 'competition_roster_id'
+  belongs_to :roster, foreign_key: 'competition_roster_id', class_name: 'CompetitionRoster'
 
   validates :user, presence: true
   validates :roster, presence: true
