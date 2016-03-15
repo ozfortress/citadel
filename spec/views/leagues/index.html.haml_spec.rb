4b2ded943dff100dddfa650772a38b1f2149c6ad
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-03-15 16:14:47 +1100

    Added tests for leagues/index.html.haml

--- spec/views/leagues/index.html.haml_spec.rb
+++ spec/views/leagues/index.html.haml_spec.rb
@@ -1,4 +1,17 @@
-require 'rails_helper'
+require 'spec_helper'
+require 'support/devise'
+require 'support/factory_girl'
 
 describe 'leagues/index.html.haml' do
+  let!(:format) { create(:format) }
+  let!(:comp1) { create(:competition, format: format, private: false) }
+  let!(:comp2) { create(:competition, format: format, private: true) }
+
+  it 'displays all leagues' do
+    render
+
+    expect(rendered).to include(format.game.name)
+    expect(rendered).to include(comp1.name)
+    expect(rendered).to_not include(comp2.name)
+  end
 end
