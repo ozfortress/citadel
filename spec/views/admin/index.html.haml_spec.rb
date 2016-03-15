21fa368eb9fc976189d2094c9f0b06d9a19c3db3
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-03-15 16:03:53 +1100

    Added tests for admin/index.html.haml

---
+++ spec/views/admin/index.html.haml_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'support/devise'
+require 'support/factory_girl'
+
+describe 'admin/index.html.haml' do
+  context 'when competitions authorized' do
+    let(:user) { create(:user) }
+
+    before do
+      user.grant(:edit, :competitions)
+    end
+
+    it 'displays username' do
+      sign_in(user)
+
+      render
+
+      expect(rendered).to include('Leagues')
+      expect(rendered).to_not include('Meta')
+    end
+  end
+
+  context 'when meta authorized' do
+    let(:user) { create(:user) }
+
+    before do
+      user.grant(:edit, :games)
+    end
+
+    it 'displays admin link' do
+      sign_in(user)
+
+      render
+
+      expect(rendered).to include('Meta')
+      expect(rendered).to_not include('Leagues')
+    end
+  end
+end
