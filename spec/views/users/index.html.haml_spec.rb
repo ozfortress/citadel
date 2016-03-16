944e6cbbb96cf9a13fd55d749d5cbe96b2a4e06f
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-03-16 15:28:54 +1100

    Added tests for users/index.html.haml

---
+++ spec/views/users/index.html.haml_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'support/devise'
+require 'support/factory_girl'
+
+describe 'users/index.html.haml' do
+  let!(:user1) { create(:user) }
+  let!(:user2) { create(:user) }
+
+  it 'shows all users' do
+    view.lookup_context.prefixes = %w[application]
+
+    render
+
+    expect(rendered).to include(user1.name)
+    expect(rendered).to include(user2.name)
+  end
+end
