b2c6cda9291dd4498bc95732b709aa1a6aee5a3c
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-03-16 15:38:29 +1100

    Added tests for users/show.html.haml

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -3,4 +3,14 @@ require 'support/devise'
 require 'support/factory_girl'
 
 describe 'users/show.html.haml' do
+  let(:user) { create(:user) }
+
+  it 'shows user data' do
+    assign(:user, user)
+
+    render
+
+    expect(rendered).to include(user.name)
+    # TODO: Add more checks for user data
+  end
 end
