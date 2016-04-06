029630e4f4e72cfe4fa758f6f511d251fe3889ec
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-04-06 10:10:48 +1000

    Added tests for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,7 @@ require 'support/omniauth'
 
 describe UsersController do
   describe 'GET #index' do
-    before do
-      create_list(:user, 50)
-    end
+    let!(:users) { create_list(:user, 50) }
 
     it 'succeeds' do
       get :index
@@ -55,9 +53,9 @@ describe UsersController do
   end
 
   describe 'GET #show' do
-    it 'succeeds' do
-      user = create(:user)
+    let(:user) { create(:user) }
 
+    it 'succeeds' do
       get :show, id: user.id
 
       expect(response).to have_http_status(:success)
@@ -65,10 +63,11 @@ describe UsersController do
   end
 
   describe 'GET #edit' do
-    it 'succeeds' do
-      user = create(:user)
+    let(:user) { create(:user) }
 
+    it 'succeeds' do
       sign_in user
+
       get :edit, id: user.id
 
       expect(response).to have_http_status(:success)
@@ -76,24 +75,105 @@ describe UsersController do
   end
 
   describe 'PATCH #update' do
+    let(:user) { create(:user, description: 'B') }
     it 'updates a user' do
-      user = create(:user, description: 'B')
-
       sign_in user
+
       patch :update, id: user.id, user: { description: 'D' }
 
-      user = User.find(user.id)
-      expect(user).to_not be_nil
-      expect(user.description).to eq('D')
+      usr = User.find(user.id)
+      expect(usr).to_not be_nil
+      expect(usr.description).to eq('D')
+    end
+  end
+
+  describe 'GET #names' do
+    let(:user) { create(:user) }
+
+    it 'succeeds for authorized user' do
+      user.grant(:edit, :users)
+      sign_in user
+
+      get :names
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects for unauthorized user' do
+      sign_in user
+
+      get :names
+
+      expect(response).to redirect_to(pages_home_path)
+    end
+
+    it 'redirects for public' do
+      get :names
+
+      expect(response).to redirect_to(pages_home_path)
+    end
+  end
+
+  describe 'POST #request_name_change' do
+    let(:user) { create(:user, name: 'A') }
+
+    it 'creates name change for authorized user' do
+      sign_in user
+      expect(user.pending_names.size).to eq(0)
+
+      post :request_name_change, id: user.id, name_change: { name: 'B' }
+
+      expect(user.pending_names.size).to eq(1)
+      name_change = user.pending_names.first
+      expect(name_change.name).to eq('B')
+      expect(name_change.approved_by).to be_nil
+      expect(name_change.denied_by).to be_nil
+    end
+
+    it 'fails for identical name' do
+      sign_in user
+
+      post :request_name_change, id: user.id, name_change: { name: 'A' }
+
+      expect(user.pending_names.size).to eq(0)
+    end
+  end
+
+  describe 'PATCH #handle_name_change' do
+    let(:user) { create(:user, name: 'A') }
+    let(:name_change) { create(:user_name_change, user: user, name: 'B') }
+    let(:admin) { create(:user) }
+    before { admin.grant(:edit, :users) }
+
+    it 'accepts name changes' do
+      sign_in admin
+
+      patch :handle_name_change, user_id: user.id, id: name_change.id, approve: true
+
+      usr = User.find(user.id)
+      expect(usr.name).to eq('B')
+      expect(usr.pending_names.size).to eq(0)
+      expect(usr.approved_names.where(name: 'B')).to exist
+    end
+
+    it 'denies name changes' do
+      sign_in admin
+
+      patch :handle_name_change, user_id: user.id, id: name_change.id, approve: false
+
+      expect(user.name).to eq('A')
+      expect(user.pending_names.size).to eq(0)
+      expect(user.approved_names.where(name: 'B')).to_not exist
     end
   end
 
   describe 'PATCH #logout' do
-    it 'logs a user out' do
-      user = create(:user)
+    let(:user) { create(:user) }
 
+    it 'logs a user out' do
       sign_in user
       request.env['HTTP_REFERER'] = '/'
+
       patch :logout
 
       expect(session['devise']).to be_nil
--- spec/factories/user_name_change.rb
+++ spec/factories/user_name_change.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :user_name_change do
     user
-    association :approved_by, factory: :user
     denied_by nil
     sequence(:name) { |n| "FOOBAR#{n}" }
   end
