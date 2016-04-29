84e7701524438361438194f3508e60576779f859
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-04-30 01:57:39 +1000

    Added tests for player counts on competition rosters

--- spec/models/competition_roster_spec.rb
+++ spec/models/competition_roster_spec.rb
@@ -19,4 +19,34 @@ describe CompetitionRoster do
 
   it { should allow_value('').for(:description) }
   it { should validate_length_of(:description).is_at_least(0) }
+
+  describe 'player count limits' do
+    it 'validates minimum players' do
+      comp = create(:competition, min_players: 1)
+      div = create(:division, competition: comp)
+
+      expect(build(:competition_roster, division: div, player_count: 1)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 5)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 0)).not_to be_valid
+    end
+
+    it 'validates maximum players' do
+      comp = create(:competition, min_players: 1, max_players: 2)
+      div = create(:division, competition: comp)
+
+      expect(build(:competition_roster, division: div, player_count: 1)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 2)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 3)).not_to be_valid
+      expect(build(:competition_roster, division: div, player_count: 5)).not_to be_valid
+    end
+
+    it 'validates no maximum on players' do
+      comp = create(:competition, min_players: 1, max_players: 0)
+      div = create(:division, competition: comp)
+
+      expect(build(:competition_roster, division: div, player_count: 1)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 10)).to be_valid
+      expect(build(:competition_roster, division: div, player_count: 20)).to be_valid
+    end
+  end
 end
