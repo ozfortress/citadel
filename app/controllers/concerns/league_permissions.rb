e9d3207d069dcb463ec32084570ec0bdec0230fd
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-08 23:45:14 +1000

    Cleaned up and fixed match permissions concern

--- app/controllers/concerns/league_permissions.rb
+++ app/controllers/concerns/league_permissions.rb
@@ -9,11 +9,4 @@ module LeaguePermissions
     user_signed_in? && (current_user.can?(:edit, @competition) ||
                         current_user.can?(:edit, :competitions))
   end
-
-  def user_can_submit_team_score?
-    status = @match.status
-
-    user_can_home_team? && status == 'submitted_by_home_team' ||
-      user_can_away_team? && status == 'submitted_by_away_team'
-  end
 end
--- app/controllers/concerns/leagues/match_permissions.rb
+++ app/controllers/concerns/leagues/match_permissions.rb
@@ -15,11 +15,15 @@ module Leagues
       user_can_edit_league? || user_can_home_team? || user_can_away_team?
     end
 
-    def can_confirm_score?
+    def user_can_confirm_score?
       status = @match.status
 
       user_can_home_team? && status == 'submitted_by_away_team' ||
         user_can_away_team? && status == 'submitted_by_home_team'
+    end
+
+    def user_can_submit_team_score?
+      @match.status == 'pending' && (user_can_home_team? || user_can_away_team?)
     end
   end
 end
