61e4516f2d88575f609b35a7a33e7dc68e5ad3e8
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-13 12:16:31 +1000

    Fixed rspec not finding helper methods in ApplicationHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,4 +52,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # RSpec doesn't respect helper hierarchy
+  config.include ApplicationHelper
 end
