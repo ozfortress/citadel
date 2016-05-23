77438e0c1eb5f33d1619f8fb1a0c03b2672283c1
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-23 14:15:19 +1000

    Added test for competition transfer model for not allows the same user in a league twice

--- spec/models/competition_transfer_spec.rb
+++ spec/models/competition_transfer_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+require 'support/shoulda'
+require 'support/factory_girl'
 
 describe CompetitionTransfer do
   before { create(:competition_transfer) }
@@ -8,4 +10,19 @@ describe CompetitionTransfer do
 
   it { should validate_presence_of(:roster) }
   it { should validate_presence_of(:user) }
+
+  it "doesn't allow a player to enter the same league twice" do
+    comp = create(:competition)
+    div1 = create(:division, competition: comp)
+    div2 = create(:division, competition: comp)
+    roster1 = create(:competition_roster, division: div1)
+    roster2 = create(:competition_roster, division: div2)
+    user = create(:user)
+    roster1.team.add_player!(user)
+    roster1.add_player!(user)
+    roster2.team.add_player!(user)
+
+    expect(build(:competition_transfer, roster: roster2, user: user,
+                                        is_joining: true)).to be_invalid
+  end
 end
