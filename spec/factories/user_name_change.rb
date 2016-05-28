87441f978823c2b14b50045e06394ea03e9547bd
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-28 14:03:48 +1000

    Added tests for users/names

--- spec/factories/user_name_change.rb
+++ spec/factories/user_name_change.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user_name_change do
     user
+    approved_by nil
     denied_by nil
-    sequence(:name) { |n| "FOOBAR#{n}" }
+    sequence(:name) { |n| "BARFOO#{n}" }
   end
 end
--- spec/views/users/names.html.haml_spec.rb
+++ spec/views/users/names.html.haml_spec.rb
@@ -1,3 +1,23 @@
 require 'rails_helper'
 
-describe 'users/names'
+describe 'users/names' do
+  let!(:name_changes) { create_list(:user_name_change, 3) }
+  let!(:approved_name_changes) { create_list(:user_name_change, 3, approved_by: build(:user)) }
+  let!(:denied_name_changes) { create_list(:user_name_change, 3, denied_by: build(:user)) }
+
+  it 'shows all pending name changes' do
+    render
+
+    name_changes.each do |name_change|
+      expect(rendered).to include(">#{name_change.name}<")
+    end
+
+    approved_name_changes.each do |name_change|
+      expect(rendered).to_not include(">#{name_change.name}<")
+    end
+
+    denied_name_changes.each do |name_change|
+      expect(rendered).to_not include(">#{name_change.name}<")
+    end
+  end
+end
