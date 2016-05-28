86b39bb324c35ea7900df5338bac93c0fca3432c
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-28 14:17:14 +1000

    Added test for pages/home view

--- spec/views/pages/home.html.haml_spec.rb
+++ spec/views/pages/home.html.haml_spec.rb
@@ -1,3 +1,17 @@
 require 'rails_helper'
+require 'support/devise'
+require 'support/factory_girl'
 
-describe 'pages/home.html.haml'
+describe 'pages/home.html.haml' do
+  let(:user) { create(:user) }
+
+  it 'renders for the unauthenticated' do
+    render
+  end
+
+  it 'renders for the authenticated' do
+    sign_in user
+
+    render
+  end
+end
