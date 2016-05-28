bfce6880958235840a2bf3b10818f349a6c407bb
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-28 14:14:59 +1000

    Added data setup to teams/show view test

--- spec/views/teams/show.html.haml_spec.rb
+++ spec/views/teams/show.html.haml_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
+require 'support/devise'
+require 'support/factory_girl'
 
 describe 'teams/show.html.haml' do
-  let(:team) { create(:team) }
-
-  # TODO: data setup
+  let!(:team) { create(:team) }
+  let!(:transfers_in) { create_list(:transfer, 5, team: team, is_joining: true) }
+  let!(:transfers_out) { create_list(:transfer, 5, team: team, is_joining: false) }
+  let!(:rosters) { create_list(:competition_roster, 3, team: team) }
 
   it 'shows public team data' do
     assign(:team, team)
@@ -11,7 +14,18 @@ describe 'teams/show.html.haml' do
     render
 
     expect(rendered).to include(team.name)
-    # TODO: Add more checks for team data
+
+    transfers_in.each do |transfer|
+      expect(rendered).to include(transfer.user.name)
+    end
+
+    transfers_out.each do |transfer|
+      expect(rendered).to include(transfer.user.name)
+    end
+
+    rosters.each do |roster|
+      expect(rendered).to include(roster.name)
+    end
   end
 
   it 'shows private team data'
