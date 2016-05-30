a4a0d911116b8f394ac69ee28904331ded92dc60
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-30 20:25:29 +1000

    Added notifications for team invitations

--- app/models/team_invite.rb
+++ app/models/team_invite.rb
@@ -1,9 +1,27 @@
 class TeamInvite < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+
   belongs_to :user
   belongs_to :team
 
   validates :user, presence: true
   validates :team, presence: true
+
+  after_create do
+    user.notify!("You have been invited to join #{team.name}.", user_path(user))
+  end
+
+  before_destroy do
+    message = if team.on_roster?(user)
+                "#{user.name} has joined #{team.name}!"
+              else
+                "#{user.name} has declined the invitation to join #{team.name}."
+              end
+
+    User.get_revokeable(:edit, team).each do |captain|
+      captain.notify!(message, user_path(user))
+    end
+  end
 
   def accept
     team.add_player!(user)
