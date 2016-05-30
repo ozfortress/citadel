058369aa69a87c204e22531b2398fa904c219cdb
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-30 21:15:34 +1000

    Added notifications for user leaving a team

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -10,4 +10,17 @@ describe Transfer do
 
   it { should validate_presence_of(:team) }
   it { should validate_presence_of(:user) }
+
+  it 'notifies captains on user leaving' do
+    team = create(:team)
+    cap1 = create(:user)
+    cap1.grant(:edit, team)
+    cap2 = create(:user)
+    cap2.grant(:edit, team)
+
+    create(:transfer, team: team, user: create(:user), is_joining: false)
+
+    expect(cap1.notifications).to_not be_empty
+    expect(cap2.notifications).to_not be_empty
+  end
 end
