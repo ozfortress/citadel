b5486f7b56514cb972278da1a3c9e99ffdf0a87d
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-30 20:05:32 +1000

    Added test for notifications on user name changes

--- spec/models/user_name_change_spec.rb
+++ spec/models/user_name_change_spec.rb
@@ -3,7 +3,7 @@ require 'support/shoulda'
 require 'support/factory_girl'
 
 describe UserNameChange do
-  let!(:user) { create(:user) }
+  let!(:name) { create(:user_name_change) }
 
   it { should belong_to(:user) }
   it { should belong_to(:approved_by).class_name('User') }
@@ -14,4 +14,10 @@ describe UserNameChange do
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
   it { should validate_length_of(:name).is_at_most(64) }
+
+  it 'notifies user on acceptance' do
+    name.update!(approved_by: name.user)
+
+    expect(name.user.notifications).to_not be_empty
+  end
 end
