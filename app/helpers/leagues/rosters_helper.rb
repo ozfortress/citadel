613dfbb5f7101b90d82224871a83899034ec095f
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-31 15:26:07 +1000

    Removed duplicate code

--- app/helpers/leagues/rosters_helper.rb
+++ app/helpers/leagues/rosters_helper.rb
@@ -1,10 +1,5 @@
 module Leagues
   module RostersHelper
     include RosterPermissions
-
-    def user_can_edit_team?
-      user_signed_in? &&
-        (current_user.can?(:edit, @roster.team) || current_user.can?(:edit, :teams))
-    end
   end
 end
