85cc9ccc72b1048d361874a0f79536a0e6aa8441
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-05-31 11:20:13 +1000

    Added folder exclusions for letsencrypt and changed API endpoint to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :pty, true
 
 # Files and folders to keep between deployments
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'vendor/bundle')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'key', 'certificates', 'challenge', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
