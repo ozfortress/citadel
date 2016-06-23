f89ed25ea6f49b1c73344a942b009fb7c9afd778
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-06-23 15:06:44 +1000

    Fixed team selection for league/matches/new form not displaying errors

--- app/helpers/leagues/matches_helper.rb
+++ app/helpers/leagues/matches_helper.rb
@@ -2,12 +2,6 @@ module Leagues
   module MatchesHelper
     include MatchPermissions
 
-    def teams_select(div)
-      div.active_rosters.map do |roster|
-        [roster.name, roster.id]
-      end
-    end
-
     def maps_select
       Map.all.map do |map|
         [map.to_s, map.id]
