f6fbd12361b905cf9c4900ac4e363b29ed1680a9
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-07-30 22:06:56 +1000

    Made matches for leagues/rosters/show ordered by creation

--- spec/views/leagues/rosters/show.html.haml_spec.rb
+++ spec/views/leagues/rosters/show.html.haml_spec.rb
@@ -1,3 +1,23 @@
 require 'rails_helper'
+require 'support/devise'
+require 'support/factory_girl'
 
-describe 'leagues/rosters/show'
+describe 'leagues/rosters/show' do
+  let(:roster) { create(:competition_roster) }
+
+  before do
+    5.times do
+      create(:competition_match, home_team: roster)
+    end
+  end
+
+  it 'shows all data' do
+    assign(:competition, roster.competition)
+    assign(:roster, roster)
+
+    render
+
+    #TODO: Check displayed data
+  end
+end
+
