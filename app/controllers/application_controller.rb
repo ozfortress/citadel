7049869a11a9a392d9f1d5834173129c1637d3e0
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-10 23:00:22 +1000

    Added ahoy_matey dependency for logging/tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :track_action
+
   protected
 
   def require_login
     redirect_back(fallback_location: root_path) unless user_signed_in?
+  end
+
+  def track_action
+    ahoy.track "#{controller_name}##{action_name}", request.filtered_parameters
   end
 end
