e0e6fea396c49ab00bd3c9565df5e33e8540456e
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-21 23:37:40 +1000

    Moved unread notifications logic to notifications model (from user)

--- app/models/user/notification.rb
+++ app/models/user/notification.rb
@@ -6,5 +6,7 @@ class User
     validates :read, inclusion: { in: [true, false] }
     validates :message, presence: true, length: { in: 1..128 }
     validates :link, presence: true, length: { in: 1..128 }
+
+    scope :unread, -> { where(read: false) }
   end
 end
