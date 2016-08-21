b9edc6a5f9d9c2a637497486ce1ff3f38cd789b5
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-21 23:38:46 +1000

    Moved notifications retrieval logic from view to controller. Added clear notifications button to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :get_notifications
+
   after_action :track_action
 
   protected
 
   def require_login
     redirect_back(fallback_location: root_path) unless user_signed_in?
+  end
+
+  def get_notifications
+    if user_signed_in?
+      @notifications = current_user.notifications.unread
+    end
   end
 
   def track_action
