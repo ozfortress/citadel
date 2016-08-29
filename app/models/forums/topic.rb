3e0db38dcbaab6e63c6d4dffe85c1c50d1aa09fc
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-30 00:46:45 +1000

    Fixed/added missing associations on Forums::Topic

--- app/models/forums/topic.rb
+++ app/models/forums/topic.rb
@@ -1,9 +1,10 @@
 module Forums
   class Topic < ApplicationRecord
+    has_ancestors   :parent_topics, key: :parent_topic_id
     belongs_to :parent_topic, class_name: 'Topic', optional: true
     belongs_to :created_by,   class_name: 'User'
 
-    has_many :child_topics, class_name: 'Topic', through: :parent_topic
+    has_many :child_topics, class_name: 'Topic', foreign_key: :parent_topic_id
     has_many :threads
 
     validates :name, presence: true, length: { in: 1..128 }
