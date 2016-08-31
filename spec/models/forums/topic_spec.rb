88e3b5e0d8ffe232fbe4e9aa52d24505b7a21205
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-08-31 13:10:08 +1000

    Added missing association tests for forums models

--- spec/models/forums/topic_spec.rb
+++ spec/models/forums/topic_spec.rb
@@ -11,6 +11,8 @@ describe Forums::Topic do
   it { should belong_to(:created_by).class_name('User') }
   it { should_not allow_value(nil).for(:created_by) }
 
+  it { should have_many(:threads).dependent(:destroy) }
+
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
   it { should validate_length_of(:name).is_at_most(128) }
