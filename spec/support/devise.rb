afc2907b9e523608ffb8e548b9e5fd2f3154dc54
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-09-02 04:24:45 +1000

    Implemented devise sign_in mocking for view tests

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,6 +1,17 @@
 require 'devise'
 
+module Devise
+  module Test
+    module ViewHelpers
+      def sign_in(user)
+        allow(view).to receive(:user_signed_in?) { true }
+        allow(view).to receive(:current_user) { user }
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::ViewHelpers, type: :view
 end
