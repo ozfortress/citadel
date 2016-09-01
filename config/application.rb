ec5a559a1254766b035696e3940d752ae8326328
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-09-02 03:28:24 +1000

    Made application autoload all lib/ code at startup

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Ozfortress
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Autoload lib/
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
