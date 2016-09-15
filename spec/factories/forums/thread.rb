e7a88a0bb080e0422242ffb3ab3266e6e9285d72
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-09-15 18:23:53 +1000

    Added forums options to factories

--- spec/factories/forums/thread.rb
+++ spec/factories/forums/thread.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :forums_thread, class: 'Forums::Thread' do
     association :topic, factory: :forums_topic
     association :created_by, factory: :user
+    locked false
+    pinned false
+    hidden false
+
     title 'Check this out'
   end
 end
--- spec/factories/forums/topic.rb
+++ spec/factories/forums/topic.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :forums_topic, class: 'Forums::Topic' do
     parent nil
     association :created_by, factory: :user
+    locked false
+    pinned false
+    hidden false
+    isolated false
+    default_hidden false
     name 'General Discussions'
   end
 end
