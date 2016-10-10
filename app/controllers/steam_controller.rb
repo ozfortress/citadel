b27e499bcb5a37e586380b251c0f691c304bffc3
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-10 22:11:23 +1100

    Handled missing players in steam controller

--- app/controllers/steam_controller.rb
+++ app/controllers/steam_controller.rb
@@ -2,6 +2,10 @@ class SteamController < ApplicationController
   before_action { @user = User.find_by(steam_id: params[:id]) }
 
   def show
-    redirect_to user_path(@user)
+    if @user
+      redirect_to user_path(@user)
+    else
+      redirect_to users_path
+    end
   end
 end
