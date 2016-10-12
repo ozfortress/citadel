58773198f4bc4dc8822d5c2f9f80c65ea841d371
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-13 00:54:32 +1100

    Fixed N+1 query on leagues/matches#index

--- app/controllers/leagues/matches_controller.rb
+++ app/controllers/leagues/matches_controller.rb
@@ -14,7 +14,7 @@ module Leagues
     before_action :require_match_not_bye, only: [:submit, :confirm, :forfeit]
 
     def index
-      @divisions = @league.divisions.includes(matches: [:home_team, :away_team, :rounds])
+      @divisions = @league.divisions.includes(matches: [:away_team, :rounds, home_team: :division])
     end
 
     def new
