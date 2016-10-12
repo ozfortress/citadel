2056a10d015881a6601a14dba69c4ada3c885193
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-12 20:29:36 +1100

    Added ability to specify unique ids for markdown editor fields

--- lib/monkey/bootstrap_form/form_builder.rb
+++ lib/monkey/bootstrap_form/form_builder.rb
@@ -3,10 +3,12 @@ require 'bootstrap_form'
 module BootstrapForm
   class FormBuilder
     def markdown_editor(method, options = {}, html_options = {})
+      id = options[:id] || method
+
       form_group_builder(method, options, html_options) do
         prepend_and_append_input(options) do
-          out = content_tag(:div, '', id: "wmd-button-bar-#{method}")
-          html_options = { class: 'wmd-input form-control', id: "wmd-input-#{method}" }
+          out = content_tag(:div, '', id: "wmd-button-bar-#{id}")
+          html_options = { class: 'wmd-input form-control', id: "wmd-input-#{id}" }
           out += text_area_without_bootstrap(method, options.merge(html_options))
           # TODO: Preview using ajax
           out
