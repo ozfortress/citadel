6ce81d616f06f1da41231594b3181f77771749fb
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-16 18:53:24 +1100

    Changed action mailer configuration to include a default-from

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Default from
+  config.action_mailer.default_options = { from: 'no-reply@demo.citadel.tv' }
+
   # Default to this host for urls in mail views
-  config.action_mailer.default_url_options = { host: "demo.citadel.tv" }
+  config.action_mailer.default_url_options = { host: 'demo.citadel.tv' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
