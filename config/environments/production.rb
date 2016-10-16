6072903ff4864a76d8d8985d64b02a4963949626
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-16 20:10:17 +1100

    Configured action mailer to use postfix in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Send using postfix, which proxies sendmail
+  config.action_mailer.delivery_method = :sendmail
+
   # Default to this host for urls in mail views
   config.action_mailer.default_url_options = { host: "warzone.ozfortress.com" }
 
