3dea668301edb8c9741d5827eab9825599ff8aa9
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-30 14:51:03 +1100

    Optimised MapsController tests

--- spec/controllers/meta/maps_controller_spec.rb
+++ spec/controllers/meta/maps_controller_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe Meta::MapsController do
-  let!(:game) { create(:game) }
-  let(:admin) { create(:user) }
+  before(:all) do
+    @game = create(:game)
 
-  before do
-    admin.grant(:edit, :games)
+    @admin = create(:user)
+    @admin.grant(:edit, :games)
+
+    @user = create(:user)
   end
 
   describe 'GET #index' do
     it 'succeeds for authorized user' do
-      sign_in admin
+      sign_in @admin
 
       get :index
 
@@ -20,7 +22,7 @@ describe Meta::MapsController do
 
   describe 'GET #new' do
     it 'succeeds for authorized user' do
-      sign_in admin
+      sign_in @admin
 
       get :new
 
@@ -30,12 +32,12 @@ describe Meta::MapsController do
 
   describe 'POST #create' do
     it 'succeeds for authorized user' do
-      sign_in admin
+      sign_in @admin
 
-      post :create, params: { map: { game_id: game.id, name: 'Foo', description: 'Bar' } }
+      post :create, params: { map: { game_id: @game.id, name: 'Foo', description: 'Bar' } }
 
       map = Map.first
-      expect(map.game).to eq(game)
+      expect(map.game).to eq(@game)
       expect(map.name).to eq('Foo')
       expect(map.description).to eq('Bar')
     end
@@ -43,45 +45,46 @@ describe Meta::MapsController do
     # TODO: Fail case
   end
 
-  describe 'GET #show' do
-    let(:map) { create(:map, game: game) }
-
-    it 'succeeds' do
-      get :show, params: { id: map.id }
-
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #edit' do
-    let(:map) { create(:map, game: game) }
-
-    it 'succeeds for authorized user' do
-      sign_in admin
-
-      get :edit, params: { id: map.id }
-
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'PATCH #update' do
-    let(:map) { create(:map, game: game) }
-    let!(:game2) { create(:game) }
-
-    it 'succeeds for authorized user' do
-      sign_in admin
-
-      patch :update, params: {
-        id: map.id, map: { game_id: game2.id, name: 'A', description: 'B' }
-      }
-
-      map = Map.first
-      expect(map.game).to eq(game2)
-      expect(map.name).to eq('A')
-      expect(map.description).to eq('B')
+  context 'existing map' do
+    before(:all) do
+      @game2 = create(:game)
     end
 
-    # TODO: Fail case
+    let(:map) { create(:map, game: @game) }
+
+    describe 'GET #show' do
+      it 'succeeds' do
+        get :show, params: { id: map.id }
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'succeeds for authorized user' do
+        sign_in @admin
+
+        get :edit, params: { id: map.id }
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'PATCH #update' do
+      it 'succeeds for authorized user' do
+        sign_in @admin
+
+        patch :update, params: {
+          id: map.id, map: { game_id: @game2.id, name: 'A', description: 'B' }
+        }
+
+        map = Map.first
+        expect(map.game).to eq(@game2)
+        expect(map.name).to eq('A')
+        expect(map.description).to eq('B')
+      end
+
+      # TODO: Fail case
+    end
   end
 end
