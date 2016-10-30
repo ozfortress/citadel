bef7540c4a50456dfaf7135afc61d92569c32714
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-10-30 14:21:02 +1100

    Expanded tests for FormatsController

--- spec/controllers/meta/formats_controller_spec.rb
+++ spec/controllers/meta/formats_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Meta::FormatsController do
   let!(:game) { create(:game) }
   let(:admin) { create(:user) }
+  let(:user) { create(:user) }
 
   before do
     admin.grant(:edit, :games)
@@ -41,9 +42,30 @@ describe Meta::FormatsController do
       expect(format.player_count).to eq(3)
       expect(format.name).to eq('Foo')
       expect(format.description).to eq('Bar')
+      expect(response).to redirect_to(meta_format_path(format))
     end
 
-    # TODO: Fail case
+    it 'fails for invalid data' do
+      sign_in admin
+
+      post :create, params: {
+        format_: { game_id: game.id, player_count: 0, name: '' }
+      }
+
+      expect(Format.all).to be_empty
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects for unauthorized user' do
+      sign_in user
+
+      post :create, params: {
+        format_: { game_id: game.id, player_count: 0, name: '' }
+      }
+
+      expect(Format.all).to be_empty
+      expect(response).to redirect_to(root_path)
+    end
   end
 
   describe 'GET #show' do
@@ -85,8 +107,31 @@ describe Meta::FormatsController do
       expect(format.player_count).to eq(1)
       expect(format.name).to eq('A')
       expect(format.description).to eq('B')
+      expect(response).to redirect_to(meta_format_path(format))
     end
 
-    # TODO: Fail case
+    it 'fails for invalid data' do
+      sign_in admin
+
+      patch :update, params: {
+        id: format.id, format_: { game_id: game2.id, player_count: 0, name: '' }
+      }
+
+      format = Format.first
+      expect(format.game).to eq(game)
+      expect(format.player_count).to_not eq(0)
+      expect(format.name).to_not eq('')
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirects for unauthorized user' do
+      sign_in user
+
+      patch :update, params: { id: format.id, format_: { game_id: game2.id } }
+
+      format = Format.first
+      expect(format.game).to eq(game)
+      expect(response).to redirect_to(root_path)
+    end
   end
 end
