c43ac487fc9c3f5e43cb1c60edcb4e163c0a45c5
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-11-07 13:14:52 +1100

    Updated fetch_ips script to also check for possible alts

--- script/fetch_ips.rb
+++ script/fetch_ips.rb
@@ -1,7 +1,21 @@
 user = User.find(ARGV[0].to_i)
 
-events = Ahoy::Event.where(user: user).reorder(nil)
+ips = Ahoy::Event.select('DISTINCT(ip)').reorder(:ip).where(user: user).map(&:ip)
 
-events.uniq.pluck(:ip).each do |ip|
+ips.each do |ip|
   puts ip
+end
+
+puts 'Possible Alts:'
+
+ips.each do |ip|
+  alt_events = Ahoy::Event.select('DISTINCT(user_id)')
+                          .reorder(:user_id)
+                          .where(ip: ip)
+                          .where.not(user: user)
+                          .includes(:user)
+
+  alt_events.map(&:user).each do |user|
+    puts "#{user.name}, ip: #{ip}, https://warzone.ozfortress.com/users/#{user.id}"
+  end
 end
