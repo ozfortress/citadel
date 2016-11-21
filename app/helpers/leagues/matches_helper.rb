299811d07eaec99cf09e8d2ab01d98d296ffe439
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-11-21 20:18:27 +1100

    Made map forms use the league's map pool when selecting maps

--- app/helpers/leagues/matches_helper.rb
+++ app/helpers/leagues/matches_helper.rb
@@ -2,12 +2,6 @@ module Leagues
   module MatchesHelper
     include MatchPermissions
 
-    def maps_select
-      Map.all.map do |map|
-        [map.to_s, map.id]
-      end
-    end
-
     def generation_select
       options_for_select [['Swiss', :swiss], ['Round Robin', :round_robin]], @kind
     end
