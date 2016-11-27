fe35a25faa08b53115114bcd11e8bb4528aecdfc
BenjaminSchaaf <ben.schaaf@gmail.com>
2016-11-28 00:40:15 +1100

    Added caching for League::Roster#won_rounds_against_tied_rosters.count, fixing N+1 Query on Leagues#show

--- app/models/league/tiebreaker.rb
+++ app/models/league/tiebreaker.rb
@@ -9,8 +9,6 @@ class League
       send("get_#{kind}", roster)
     end
 
-    private
-
     def get_round_wins(roster)
       roster.won_rounds_count
     end
@@ -20,21 +18,11 @@ class League
     end
 
     def get_round_wins_against_tied_rosters(roster)
-      tied_rosters = get_tied_rosters_for(roster)
-      return 0 unless tied_rosters.exists?
-
-      won_rounds = roster.won_rounds.joins(:match)
-      won_rounds.where(league_matches: { home_team_id: tied_rosters })
-                .union(won_rounds.where(league_matches: { away_team_id: tied_rosters }))
-                .count
+      roster.won_rounds_against_tied_rosters_count
     end
 
     def get_median_bucholz_score(roster)
       roster.won_rounds_count * 1.0 + roster.drawn_rounds_count * 0.5
-    end
-
-    def get_tied_rosters_for(roster)
-      roster.division.rosters.active.where(points: roster.points).where.not(id: roster.id)
     end
   end
 end
