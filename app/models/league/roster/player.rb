7c40daec258e5e5341fccd5bde84773d4e7c1a77
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-12 15:26:46 +1100

    Added error target to base for League::Player#unique_within_league validation

--- app/models/league/roster/player.rb
+++ app/models/league/roster/player.rb
@@ -22,7 +22,7 @@ class League
         return unless user.present? && roster.present?
 
         if league.players.where(user: user).where.not(id: id).exists?
-          errors.add(:user_id, 'can only be in one roster per league')
+          errors.add(:base, 'can only be in one roster per league')
         end
       end
     end
