d8edb275066d4f134dcf701d27c00a5dc0191f00
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-14 19:01:33 +1100

    Massively expanded test coverage for teams controller

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -34,6 +34,7 @@ describe TeamsController do
   describe 'POST #create' do
     it 'creates a team' do
       sign_in user
+
       post :create, params: { team: { name: 'A', description: 'B' } }
 
       team = Team.find_by(name: 'A')
@@ -44,179 +45,268 @@ describe TeamsController do
     end
 
     it 'handles duplicate teams' do
-      create(:team, name: 'A')
-
+      team = create(:team, name: 'A', description: 'A')
       sign_in user
+
       post :create, params: { team: { name: 'A', description: 'B' } }
+
+      expect(Team.count).to eq(1)
     end
   end
 
-  describe 'GET #show' do
-    it 'succeeds' do
-      team = create(:team)
-
-      get :show, params: { id: team.id }
-
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #edit' do
-    it 'succeeds' do
-      team = create(:team)
-      user.grant(:edit, team)
-
-      sign_in user
-      get :edit, params: { id: team.id }
-
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'PATCH #update' do
+  context 'existing team' do
     let(:team) { create(:team_with_avatar, name: 'A', description: 'B') }
 
-    it 'updates a team' do
-      user.grant(:edit, team)
-      sign_in user
+    describe 'GET #show' do
+      it 'succeeds' do
+        get :show, params: { id: team.id }
 
-      patch :update, params: { id: team.id, team: { name: 'C', description: 'D' } }
-
-      team.reload
-      expect(team.name).to eq('C')
-      expect(team.description).to eq('D')
-      expect(response).to redirect_to(team_path(team))
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'allows avatar removal' do
-      user.grant(:edit, team)
-      sign_in user
+    describe 'GET #edit' do
+      it 'succeeds' do
+        user.grant(:edit, team)
+        sign_in user
 
-      patch :update, params: { id: team.id, team: { remove_avatar: true } }
+        get :edit, params: { id: team.id }
 
-      team.reload
-      expect(team.avatar.url).to eq(team.avatar.default_url)
-      expect(response).to redirect_to(team_path(team))
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'fails for invalid data' do
-      user.grant(:edit, team)
-      sign_in user
+    describe 'PATCH #update' do
+      it 'updates a team' do
+        user.grant(:edit, team)
+        sign_in user
 
-      patch :update, params: { id: team.id, team: { name: '', description: '' } }
+        patch :update, params: { id: team.id, team: { name: 'C', description: 'D' } }
 
-      team.reload
-      expect(team.name).to eq('A')
-      expect(team.description).to eq('B')
-      expect(response).to have_http_status(:success)
-    end
-  end
+        team.reload
+        expect(team.name).to eq('C')
+        expect(team.description).to eq('D')
+        expect(response).to redirect_to(team_path(team))
+      end
 
-  describe 'GET #recruit' do
-    before { create_list(:user, 20) }
+      it 'allows avatar removal' do
+        user.grant(:edit, team)
+        sign_in user
 
-    it 'succeeds' do
-      team = create(:team)
-      user.grant(:edit, team)
+        patch :update, params: { id: team.id, team: { remove_avatar: true } }
 
-      sign_in user
-      get :recruit, params: { id: team.id }
+        team.reload
+        expect(team.avatar.url).to eq(team.avatar.default_url)
+        expect(response).to redirect_to(team_path(team))
+      end
 
-      expect(response).to have_http_status(:success)
-    end
-  end
+      it 'fails for invalid data' do
+        user.grant(:edit, team)
+        sign_in user
 
-  describe 'PATCH #invite' do
-    it 'invites a player to a team' do
-      team = create(:team)
-      invited = create(:user)
-      user.grant(:edit, team)
+        patch :update, params: { id: team.id, team: { name: '', description: '' } }
 
-      sign_in user
-      patch :invite, params: { id: team.id, user_id: invited.id }
+        team.reload
+        expect(team.name).to eq('A')
+        expect(team.description).to eq('B')
+        expect(response).to have_http_status(:success)
+      end
 
-      invite = invited.team_invites.first
-      expect(invite).to_not be_nil
-      expect(invite.user).to eq(invited)
-      expect(invite.team).to eq(team)
-      expect(team.invited?(invited)).to be(true)
-      expect(invited.notifications).to_not be_empty
-      expect(user.notifications).to be_empty
-    end
-  end
+      it 'redirects for unauthorized user' do
+        sign_in user
 
-  describe 'PATCH #kick' do
-    it 'removes a player from a team' do
-      team = create(:team)
-      player = create(:user)
-      team.add_player!(player)
-      user.grant(:edit, team)
-      sign_in user
+        patch :update, params: { id: team.id, team: { name: 'C', description: 'D' } }
 
-      patch :kick, params: { id: team.id, user_id: player.id }
+        team.reload
+        expect(team.name).to eq('A')
+        expect(team.description).to eq('B')
+        expect(response).to redirect_to(team_path(team))
+      end
 
-      expect(team.on_roster?(player)).to be(false)
-      expect(team.invited?(player)).to be(false)
-      expect(player.notifications).to_not be_empty
-      expect(user.notifications).to be_empty
-      expect(response).to redirect_to(team_path(team))
-    end
-  end
+      it 'redirects for unauthenticated user' do
+        patch :update, params: { id: team.id, team: { name: 'C', description: 'D' } }
 
-  describe 'PATCH #leave' do
-    it 'removes a player from a team' do
-      team = create(:team)
-      team.add_player!(user)
-      captain = create(:user)
-      captain.grant(:edit, team)
-      sign_in user
-
-      patch :leave, params: { id: team.id }
-
-      team.reload
-      expect(team.on_roster?(user)).to be(false)
-      expect(team.invited?(user)).to be(false)
-      expect(user.notifications).to be_empty
-      expect(captain.notifications).to_not be_empty
-      expect(response).to redirect_to(team_path(team))
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    let(:team) { create(:team) }
-    let(:user) { create(:user) }
-    let(:invited) { create(:user) }
-
-    before do
-      team.add_player!(user)
-      invited.grant(:edit, team)
-      team.invite(invited)
+        team.reload
+        expect(team.name).to eq('A')
+        expect(team.description).to eq('B')
+        expect(response).to redirect_to(team_path(team))
+      end
     end
 
-    it 'succeeds for authorized user' do
-      user.grant(:edit, :teams)
-      sign_in user
+    describe 'GET #recruit' do
+      before { create_list(:user, 20) }
 
-      delete :destroy, params: { id: team.id }
+      it 'succeeds' do
+        user.grant(:edit, team)
+        sign_in user
 
-      expect(Team.all).to be_empty
+        get :recruit, params: { id: team.id }
+
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'succeeds for authorized captains' do
-      invited.grant(:edit, team)
-      sign_in invited
+    describe 'PATCH #invite' do
+      let(:invited) { create(:user) }
 
-      delete :destroy, params: { id: team.id }
+      it 'invites a player to a team' do
+        user.grant(:edit, team)
+        sign_in user
 
-      expect(Team.all).to be_empty
+        patch :invite, params: { id: team.id, user_id: invited.id }
+
+        invite = invited.team_invites.first
+        expect(invite).to_not be_nil
+        expect(invite.user).to eq(invited)
+        expect(invite.team).to eq(team)
+        expect(team.invited?(invited)).to be(true)
+        expect(invited.notifications).to_not be_empty
+        expect(user.notifications).to be_empty
+      end
+
+      it 'redirects for unauthorized user' do
+        sign_in user
+
+        patch :invite, params: { id: team.id, user_id: invited.id }
+
+        expect(invited.team_invites).to be_empty
+        expect(invited.notifications).to be_empty
+        expect(response).to redirect_to(team_path(team))
+      end
+
+      it 'redirects for unauthenticated user' do
+        patch :invite, params: { id: team.id, user_id: invited.id }
+
+        expect(invited.team_invites).to be_empty
+        expect(invited.notifications).to be_empty
+        expect(response).to redirect_to(team_path(team))
+      end
     end
 
-    it 'fails for unauthorized user' do
-      sign_in user
+    context 'player on team' do
+      let(:player) { create(:user) }
 
-      delete :destroy, params: { id: team.id }
+      before do
+        team.add_player!(player)
+      end
 
-      expect(Team.where(id: team.id)).to exist
+      describe 'PATCH #kick' do
+        it 'succeeds for captain' do
+          user.grant(:edit, team)
+          sign_in user
+
+          patch :kick, params: { id: team.id, user_id: player.id }
+
+          expect(team.on_roster?(player)).to be(false)
+          expect(team.invited?(player)).to be(false)
+          expect(player.notifications).to_not be_empty
+          expect(user.notifications).to be_empty
+          expect(response).to redirect_to(team_path(team))
+        end
+
+        it 'fails for player' do
+          sign_in player
+
+          patch :kick, params: { id: team.id, user_id: player.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+
+        it 'fails for unauthorized user' do
+          sign_in user
+
+          patch :kick, params: { id: team.id, user_id: player.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+
+        it 'fails for unauthenticated user' do
+          patch :kick, params: { id: team.id, user_id: player.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+      end
+
+      describe 'PATCH #leave' do
+        it 'succeeds for player' do
+          user.grant(:edit, team)
+          sign_in player
+
+          patch :leave, params: { id: team.id }
+
+          team.reload
+          expect(team.on_roster?(player)).to be(false)
+          expect(team.invited?(player)).to be(false)
+          expect(player.notifications).to be_empty
+          expect(user.notifications).to_not be_empty
+          expect(response).to redirect_to(team_path(team))
+        end
+
+        it 'fails for captain' do
+          user.grant(:edit, team)
+          sign_in user
+
+          patch :leave, params: { id: team.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+
+        it 'fails for other users' do
+          sign_in user
+
+          patch :leave, params: { id: team.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+
+        it 'fails for unauthenticated user' do
+          patch :leave, params: { id: team.id }
+
+          expect(team.on_roster?(player)).to be(true)
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      let(:invited) { create(:user) }
+
+      before do
+        team.add_player!(user)
+        invited.grant(:edit, team)
+        team.invite(invited)
+      end
+
+      it 'succeeds for authorized user' do
+        user.grant(:edit, :teams)
+        sign_in user
+
+        delete :destroy, params: { id: team.id }
+
+        expect(Team.all).to be_empty
+      end
+
+      it 'succeeds for authorized captains' do
+        invited.grant(:edit, team)
+        sign_in invited
+
+        delete :destroy, params: { id: team.id }
+
+        expect(Team.all).to be_empty
+      end
+
+      it 'fails for unauthorized user' do
+        sign_in user
+
+        delete :destroy, params: { id: team.id }
+
+        expect(Team.where(id: team.id)).to exist
+      end
+
+      it 'fails for unauthenticated user' do
+        delete :destroy, params: { id: team.id }
+
+        expect(Team.where(id: team.id)).to exist
+      end
     end
   end
 end
