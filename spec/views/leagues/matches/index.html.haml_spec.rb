522e2efff0a144cb6318e3ab0cc0a4ef9abbb793
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-23 00:22:55 +1100

    Fixed leagues/matches/index test not properly mocking matches and therefore not covering a lot of presenter logic

--- spec/views/leagues/matches/index.html.haml_spec.rb
+++ spec/views/leagues/matches/index.html.haml_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 describe 'leagues/matches/index' do
   let(:div) { build(:league_division) }
-  let(:home_team) { build(:league_roster) }
-  let(:away_team) { build(:league_roster) }
+  let(:home_team) { build_stubbed(:league_roster) }
+  let(:away_team) { build_stubbed(:league_roster) }
 
   before do
     @matches = []
     @matches << build_stubbed(:league_match, home_team: home_team,
                                              away_team: away_team, status: 'confirmed')
-    @matches << build_stubbed(:league_match, home_team: home_team, away_team: nil)
+    @matches << build_stubbed(:league_match, home_team: home_team, away_team: away_team,
+                                             status: 'pending', round_name: 'Finals')
+    @matches << build_stubbed(:bye_league_match, home_team: home_team, status: 'confirmed')
     League::Match.forfeit_bies.each do |ff, _|
-      @matches << build_stubbed(:league_match, home_team: home_team,
-                                               away_team: away_team, forfeit_by: ff)
+      @matches << build_stubbed(:league_match, home_team: home_team, away_team: away_team,
+                                               forfeit_by: ff, status: 'confirmed')
     end
 
     allow(div).to receive(:matches).and_return(@matches)
@@ -27,6 +29,7 @@ describe 'leagues/matches/index' do
 
     @matches.each do |match|
       expect(rendered).to include(match.home_team.name)
+      expect(rendered).to include(match.away_team.name) if match.away_team
     end
   end
 end
