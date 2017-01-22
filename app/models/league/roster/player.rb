6b8085687de4c7177365c89fc77b6399fae3b07a
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-22 12:58:24 +1100

    Added validations to roster players and roster transfer requests for ensuring banned users cannot join rosters

--- app/models/league/roster/player.rb
+++ app/models/league/roster/player.rb
@@ -7,6 +7,7 @@ class League
       delegate :league, :division, to: :roster
 
       validate :unique_within_league
+      validate :league_permissions, on: :create
 
       after_create do
         roster.transfers.create(user: user, is_joining: true)
@@ -24,6 +25,12 @@ class League
         if league.players.where(user: user).where.not(id: id).exists?
           errors.add(:base, 'can only be in one roster per league')
         end
+      end
+
+      def league_permissions
+        return unless user.present?
+
+        errors.add(:base, 'user is banned from leagues') unless user.can?(:use, :leagues)
       end
     end
   end
--- app/models/league/roster/transfer_request.rb
+++ app/models/league/roster/transfer_request.rb
@@ -12,6 +12,7 @@ class League
       validate :on_team,   if: :is_joining?
       validate :on_roster, unless: :is_joining?
       validate :active_roster, if: :is_joining?
+      validate :league_permissions, if: :is_joining?
       validate :within_roster_size_limits
       validate :within_roster_size_limits_for_leaving_roster, if: :is_joining?
 
@@ -64,6 +65,12 @@ class League
         return unless roster.present?
 
         errors.add(:base, 'cannot join a disbanded roster') if roster.disbanded?
+      end
+
+      def league_permissions
+        return unless user.present?
+
+        errors.add(:base, 'user is banned from leagues') unless user.can?(:use, :leagues)
       end
 
       def within_roster_size_limits
