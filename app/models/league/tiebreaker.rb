6c845117d8a2aee52d5a173348c36b3a07744361
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-27 17:43:18 +1100

    Reworked the way rosters are sorted, moving the actual sorting from ruby to postgres Should marginally improve performance

--- app/models/league/tiebreaker.rb
+++ app/models/league/tiebreaker.rb
@@ -5,24 +5,44 @@ class League
     enum kind: [:round_wins, :round_score_difference, :round_wins_against_tied_rosters,
                 :median_bucholz_score]
 
-    def get_comparison(roster)
-      send("get_#{kind}", roster)
+    def value_for(roster)
+      send("#{kind}_value_for", roster)
     end
 
-    def get_round_wins(roster)
+    def order
+      send("#{kind}_order")
+    end
+
+    def round_wins_value_for(roster)
       roster.won_rounds_count
     end
 
-    def get_round_score_difference(roster)
+    def round_wins_order
+      'won_rounds_count DESC'
+    end
+
+    def round_score_difference_value_for(roster)
       roster.total_scores
     end
 
-    def get_round_wins_against_tied_rosters(roster)
+    def round_score_difference_order
+      'total_scores DESC'
+    end
+
+    def round_wins_against_tied_rosters_value_for(roster)
       roster.won_rounds_against_tied_rosters_count
     end
 
-    def get_median_bucholz_score(roster)
+    def round_wins_against_tied_rosters_order
+      'won_rounds_against_tied_rosters_count DESC'
+    end
+
+    def median_bucholz_score_value_for(roster)
       roster.won_rounds_count * 1.0 + roster.drawn_rounds_count * 0.5
+    end
+
+    def median_bucholz_score_order
+      '(won_rounds_count * 1.0 + drawn_rounds_count * 0.5) DESC'
     end
   end
 end
