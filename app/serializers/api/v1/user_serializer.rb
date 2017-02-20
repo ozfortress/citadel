aa67b5e3d1ccecde5d9ca9e0d560097042921821
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-20 15:07:19 +1100

    Fixed user serializer using the wrong avatar url

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -5,7 +5,7 @@ module API
 
       attributes :id, :name, :description, :created_at
       attributes :steam_32, :steam_64, :steam_id3
-      attribute(:profile_url) { object.avatar.icon.url }
+      attribute(:profile_url) { object.avatar.thumb.url }
 
       has_many :rosters, serializer: V1::Leagues::RosterSerializer
     end
