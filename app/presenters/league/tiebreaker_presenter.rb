8c79ed9142f0a3dc6515fb2c990722cf14155614
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-25 15:54:32 +1100

    Added tooltips to league tiebreakers for league points table. Fixes #188.

--- app/presenters/league/tiebreaker_presenter.rb
+++ app/presenters/league/tiebreaker_presenter.rb
@@ -6,11 +6,26 @@ class League
       'round_wins'                      => 'Wins',
       'round_score_difference'          => 'Total Score',
       'round_wins_against_tied_rosters' => 'Tied Wins',
-      'median_bucholz_score'            => 'Buchholz'
+      'median_bucholz_score'            => 'Buchholz',
+    }.freeze
+
+    TITLES = {
+      'round_wins'                      => 'Total number of rounds won',
+      'round_score_difference'          => 'Sum of scores of all rounds',
+      'round_wins_against_tied_rosters' => 'Rounds won against tied teams',
+      'median_bucholz_score'            => 'Median Buchholz score',
     }.freeze
 
     def name
       NAMES[tiebreaker.kind]
+    end
+
+    def title
+      TITLES[tiebreaker.kind]
+    end
+
+    def name_with_tooltip
+      content_tag(:div, name, data: { toggle: :tooltip, placement: :bottom }, title: title)
     end
   end
 end
