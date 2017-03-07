8616ead56036a3644bc39253b974a50ec2f7d8e2
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-07 12:47:30 +1100

    Implemented clean_logs script for truncating and cleaning analytics data

---
+++ script/clean_logs.rb
@@ -0,0 +1,29 @@
+BACKLOG = 1.month
+
+VISIT_EXACT_COLUMNS = [:ip, :user_agent, :browser, :os].freeze
+VISIT_DISTINCT_COLUMNS = [:user_id, :api_key_id].freeze
+
+keep_time = Time.zone.now - BACKLOG
+
+Visit.transaction do
+  # Delete all events beyond the backlog
+  deleted = Ahoy::Event.where('time < ?', keep_time).delete_all
+  puts "Deleted #{deleted} event records"
+
+  # De-duplicate visits beyond the backlog
+  conditions = VISIT_EXACT_COLUMNS.map { |name| "visits.#{name} = t2.#{name}" }
+  op = 'IS NOT DISTINCT FROM'
+  conditions += VISIT_DISTINCT_COLUMNS.map { |name| "visits.#{name} #{op} t2.#{name}" }
+  condition = conditions.join(' AND ')
+
+  deleted = Visit.connection.execute("
+              DELETE FROM visits
+              WHERE started_at < #{ActiveRecord::Base.sanitize(keep_time)} AND EXISTS(
+                SELECT 1 FROM visits AS t2
+                WHERE #{condition} AND t2.id > visits.id
+              )").cmd_tuples
+
+  puts "Deleted #{deleted} duplicate visit records"
+
+  raise ActiveRecord::Rollback
+end
