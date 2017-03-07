9e3345998ca84695592449b505353b8150117280
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-07 12:46:15 +1100

    Updated fetch_ips script for recent visit updates

--- script/fetch_ips.rb
+++ script/fetch_ips.rb
@@ -1,6 +1,6 @@
 user = User.find(ARGV[0].to_i)
 
-ips = Ahoy::Event.select('DISTINCT(ip)').reorder(:ip).where(user: user).map(&:ip)
+ips = Visit.select('DISTINCT(ip)').reorder(:ip).where(user: user).map(&:ip)
 
 ips.each do |ip|
   puts ip
@@ -8,14 +8,14 @@ end
 
 puts 'Possible Alts:'
 
-ips.each do |ip|
-  alt_events = Ahoy::Event.select('DISTINCT(user_id)')
-                          .reorder(:user_id)
-                          .where(ip: ip)
-                          .where.not(user: user)
-                          .includes(:user)
+alt_visits = Visit.select('DISTINCT(user_id)')
+                  .reorder(:user_id)
+                  .where(ip: ips)
+                  .where.not(user: user)
+                  .includes(:user)
 
-  alt_events.map(&:user).each do |alt|
-    puts "#{alt.name}, ip: #{ip}, https://warzone.ozfortress.com/users/#{alt.id}"
-  end
+alt_visits.each do |visit|
+  alt = visit.user
+
+  puts "#{alt.name}, ip: #{visit.ip}, https://warzone.ozfortress.com/users/#{alt.id}"
 end
