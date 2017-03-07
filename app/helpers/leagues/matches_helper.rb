2ca1d0889e65871c0fc3426ff8dd96a009d47219
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-07 14:03:16 +1100

    Rubocop, reworked verify.ozfortress.com link

--- app/helpers/leagues/matches_helper.rb
+++ app/helpers/leagues/matches_helper.rb
@@ -6,5 +6,11 @@ module Leagues
     def generation_select
       options_for_select [['Swiss', :swiss], ['Round Robin', :round_robin]], @kind
     end
+
+    def verify_rosters_link(match)
+      params = { link: match_url(match) }
+
+      "https://verify.ozfortress.com?#{params.to_param}"
+    end
   end
 end
