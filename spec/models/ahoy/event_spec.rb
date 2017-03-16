b23950a2de388186f83688a2b5262f98a7657570
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-16 15:28:03 +1100

    Removed dead test

--- spec/models/ahoy/event_spec.rb
+++ spec/models/ahoy/event_spec.rb
@@ -5,16 +5,4 @@ describe Ahoy::Event do
 
   it { should belong_to(:visit) }
   it { should allow_value(nil).for(:visit) }
-
-  describe '#search' do
-    it 'can perform search' do
-      user = create(:user)
-      ip_visit = create(:visit, user: user, ip: '12.34.56.78')
-      ip_events = create_list(:ahoy_event, 5, visit: ip_visit)
-      create_list(:ahoy_event, 5)
-
-      expect(Ahoy::Event.search('12.34.56.78')).to eq(ip_events)
-      expect(Ahoy::Event.search('').count).to be >= 10
-    end
-  end
 end
