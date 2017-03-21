4a0902752501caa3b6c346a5da49242338c061b9
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-22 01:21:54 +1100

    Added `steam_64_str` attribute for user API resources. Fixes #207

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -5,6 +5,7 @@ module API
 
       attributes :id, :name, :description, :created_at
       attributes :steam_32, :steam_64, :steam_id3
+      attribute(:steam_64_str) { object.steam_64.to_s } # For dumb json implementations
       attribute(:profile_url) { object.avatar.thumb.url }
 
       has_many :rosters, serializer: V1::Leagues::RosterSerializer
