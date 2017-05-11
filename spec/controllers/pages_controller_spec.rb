803de5efbeb5a5eb38e5b88c653ccd65640b75ed
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-05-11 12:42:29 +1000

    Expanded tests for home news page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,12 +2,37 @@ require 'rails_helper'
 
 describe PagesController do
   describe 'GET #home' do
-    it 'returns http success' do
+    after do
       Rails.configuration.news['type'] = 'none'
+    end
 
-      get :home
+    context 'news: none' do
+      it 'returns http success' do
+        Rails.configuration.news['type'] = 'none'
 
-      expect(response).to have_http_status(:success)
+        get :home
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'news: topic' do
+      it 'returns http success' do
+        Rails.configuration.news['type'] = 'topic'
+        Rails.configuration.news['id'] = create(:forums_topic).id
+
+        get :home
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'invalid news config' do
+      it 'raises internal error' do
+        Rails.configuration.news['type'] = nil
+
+        expect { get :home }.to raise_error(UncaughtThrowError)
+      end
     end
   end
 end
