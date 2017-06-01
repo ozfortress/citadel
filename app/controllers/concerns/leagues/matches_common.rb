70aa4ec6db9ba06640dce6de0eaaccefe6de5a77
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-06-01 14:58:01 +1000

    Fixed primary N+1 Query issues on match pages. Fixes #231.

--- app/controllers/concerns/leagues/matches_common.rb
+++ app/controllers/concerns/leagues/matches_common.rb
@@ -8,6 +8,18 @@ module Leagues
       @rounds    = @match.rounds.includes(:map)
 
       @comms = @match.comms.order(:created_at).includes(:user)
+
+      match_show_permissions_includes
+    end
+
+    def match_show_permissions_includes
+      home_team_permissions = User.permissions(@match.home_team.users.load)
+      home_team_permissions.fetch(:edit, @match.home_team.team)
+      home_team_permissions.fetch(:use, @match.home_team.team)
+
+      away_team_permissions = User.permissions(@match.away_team.users.load)
+      away_team_permissions.fetch(:edit, @match.away_team.team)
+      away_team_permissions.fetch(:use, @match.away_team.team)
     end
   end
 end
