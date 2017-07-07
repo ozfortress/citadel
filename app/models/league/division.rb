4b4e2d74abe72fadcd53b7dbb09b4c6fcadf67e6
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-07-07 10:29:55 +1000

    Fixed match generation using page playoff system.

--- app/models/league/division.rb
+++ app/models/league/division.rb
@@ -61,7 +61,9 @@ class League
     def generate_page_playoffs(match_options, options)
       match_options[:has_winner] = true
       driver_options = options.slice(:starting_round)
-      Tournament::SingleElimination.generate new_driver(match_options, driver_options)
+      driver_options[:teams_limit] = 4
+
+      Tournament::PagePlayoff.generate new_driver(match_options, driver_options)
     end
   end
 end
