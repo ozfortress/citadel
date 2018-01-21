bf1da06a133da6a79bd53f8dff14e464b4a7bccf
Alex <a.jbutler019@gmail.com>
2018-01-21 18:45:21 +1000

    Added read-only ban lookup to v1 API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,6 +12,22 @@ module API
 
         render json: @user, serializer: UserSerializer
       end
+
+      def bans
+        @user = User.find(params[:id])
+
+        @ban_models = User.ban_models.map do |_action, bans|
+          bans.map do |subject, model|
+            next if model.subject?
+
+            model
+          end
+        end.reduce(:+).compact.sort_by(&:association_name)
+        @bans = @ban_models.map { |model| model.where(user: @user).to_a }.reduce(:+).sort_by(&:created_at)
+        @new_bans = @ban_models.map(&:new)
+
+        render json: @bans, each_serializer: BanSerializer
+      end
     end
   end
 end
