c80d7a6c0096e23d79051764dc7aa483d661a55a
Mikhail <tradecontent@gmail.com>
2018-08-11 15:54:46 +0800

    Notification model specs

--- spec/models/user/notification_spec.rb
+++ spec/models/user/notification_spec.rb
@@ -7,10 +7,6 @@ describe User::Notification do
   it { should_not allow_value(nil).for(:user) }
 
   it { should validate_presence_of(:message) }
-  it { should validate_length_of(:message).is_at_least(1) }
-  it { should validate_length_of(:message).is_at_most(128) }
 
   it { should validate_presence_of(:link) }
-  it { should validate_length_of(:link).is_at_least(1) }
-  it { should validate_length_of(:link).is_at_most(128) }
 end
