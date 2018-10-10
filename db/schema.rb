308fc12590cbaa174d407befb321db7f0286fb81
Benjamin Schaaf <ben.schaaf@gmail.com>
2018-10-10 19:04:58 +1100

    Add list of forums posts to user profiles. Fixes #401

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_093304) do
+ActiveRecord::Schema.define(version: 2018_10_07_225216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -625,6 +625,8 @@ ActiveRecord::Schema.define(version: 2018_10_06_093304) do
     t.text "notice", default: "", null: false
     t.text "notice_render_cache", default: "", null: false
     t.string "avatar_token"
+    t.integer "forums_posts_count", default: 0, null: false
+    t.integer "public_forums_posts_count", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["query_name_cache"], name: "index_users_on_query_name_cache", opclass: :gist_trgm_ops, using: :gist
