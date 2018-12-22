15a3f6b550f67cf4f72a93a6a8f2655d9ee7030c
Benjamin Schaaf <ben.schaaf@gmail.com>
2018-12-22 23:43:56 +1100

    Fix notification updates still being requested when user is not logged in

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -8,3 +8,12 @@ Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   # Keep track of the ip in the session so we can detect address changes
   auth.session[:ip] = ip
 end
+
+# Make login state accessible from JS
+Warden::Manager.after_set_user do |user,auth,opts|
+  auth.cookies[:signed_in] = 1
+end
+
+Warden::Manager.before_logout do |user,auth,opts|
+  auth.cookies.delete :signed_in
+end
