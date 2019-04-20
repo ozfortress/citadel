e568e52cae6f6280e8bf3e8aaed20a529993a1cc
Benjamin Schaaf <ben.schaaf@gmail.com>
2019-04-20 20:55:56 +0200

    Rubocop fix

--- script/medals_dump.rb
+++ script/medals_dump.rb
@@ -12,15 +12,15 @@ end
 league.divisions.each do |division|
   rosters = division.rosters.ordered.active
 
-  fail if rosters.first.ranking.nil?
+  raise if rosters.first.ranking.nil?
   dump_roster rosters.first
   puts
 
-  fail if rosters.second.ranking.nil?
+  raise if rosters.second.ranking.nil?
   dump_roster rosters.second
   puts
 
-  fail if rosters.third.ranking.nil?
+  raise if rosters.third.ranking.nil?
   dump_roster rosters.third
   puts
   rosters[3..rosters.length].each { |roster| dump_roster roster }
