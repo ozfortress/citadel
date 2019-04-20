8c9fe703a13044fc2bf894a37c7be4c5683afb05
Benjamin Schaaf <ben.schaaf@gmail.com>
2019-04-20 20:50:02 +0200

    Fix medals_dump script

--- script/medals_dump.rb
+++ script/medals_dump.rb
@@ -10,12 +10,17 @@ def dump_roster(roster)
 end
 
 league.divisions.each do |division|
-  rosters = division.rosters.ordered(league).active
+  rosters = division.rosters.ordered.active
 
+  fail if rosters.first.ranking.nil?
   dump_roster rosters.first
   puts
+
+  fail if rosters.second.ranking.nil?
   dump_roster rosters.second
   puts
+
+  fail if rosters.third.ranking.nil?
   dump_roster rosters.third
   puts
   rosters[3..rosters.length].each { |roster| dump_roster roster }
