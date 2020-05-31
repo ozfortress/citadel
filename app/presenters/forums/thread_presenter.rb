7f6d65b352af22946d5763c02a06fae4cd2ed262
Bryan Choo <bryan.choo@commonground.work>
2020-06-01 01:04:23 +0800

    Update forum listing styling and refactor action button layouts

--- app/presenters/forums/thread_presenter.rb
+++ app/presenters/forums/thread_presenter.rb
@@ -2,13 +2,21 @@ module Forums
   class ThreadPresenter < BasePresenter
     presents :thread
 
-    def link
-      link_to(thread.title, forums_thread_path(thread))
+    def link(options = {})
+      link_to(thread.title, forums_thread_path(thread), options)
     end
 
     def breadcrumbs
       crumbs = path_objects.map { |path| content_tag(:li, path, class: 'breadcrumb-item') }
       safe_join(crumbs, '')
+    end
+
+    def status_icons
+      icons = []
+      icons << locked_icon if thread.locked
+      icons << hidden_icon if thread.hidden
+      icons << pinned_icon if thread.pinned
+      safe_join(icons, '')
     end
 
     def to_s
@@ -19,6 +27,18 @@ module Forums
 
     def path_objects
       present_collection(thread.path).map(&:link)
+    end
+
+    def locked_icon
+      inline_svg_tag('open_iconic/lock-locked.svg', title: 'Locked', class: 'icon icon-locked mr-1')
+    end
+
+    def hidden_icon
+      inline_svg_tag('svgrepo/hide.svg', title: 'Hidden', class: 'icon icon-hidden mr-1')
+    end
+
+    def pinned_icon
+      inline_svg_tag('open_iconic/pin.svg', title: 'Pinned', class: 'icon icon-pinned mr-1')
     end
   end
 end
