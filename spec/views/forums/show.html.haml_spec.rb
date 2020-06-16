937dabc055da85ec2af3c5f722804f6953a32aee
Benjamin Schaaf <ben.schaaf@gmail.com>
2020-06-17 00:55:13 +1000

    Fix more tests

--- spec/views/forums/show.html.haml_spec.rb
+++ spec/views/forums/show.html.haml_spec.rb
@@ -5,6 +5,10 @@ describe 'forums/show' do
   let(:topics) { build_stubbed_list(:forums_topic, 20) }
 
   it 'displays' do
+    threads.each do |thread|
+      allow(thread).to receive(:latest_post) { build_stubbed(:forums_post, thread: thread) }
+    end
+
     allow(view).to receive(:user_can_manage_thread?).and_return(true)
     assign(:threads, threads)
     assign(:topics, topics)
