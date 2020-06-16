96487122962787853eb7f467632c21a3c89e804a
Benjamin Schaaf <ben.schaaf@gmail.com>
2020-06-17 00:46:28 +1000

    Fix tests

--- spec/views/forums/topics/show.html.haml_spec.rb
+++ spec/views/forums/topics/show.html.haml_spec.rb
@@ -6,6 +6,10 @@ describe 'forums/topics/show' do
   let(:subtopics) { build_stubbed_list(:forums_topic, 5, parent: topic) }
 
   it 'displays data' do
+    threads.each do |thread|
+      allow(thread).to receive(:latest_post) { build_stubbed(:forums_post, thread: thread) }
+    end
+
     allow(view).to receive(:user_can_manage_topic?).and_return(true)
     assign(:topic, topic)
     assign(:threads, threads.paginate(page: 1))
