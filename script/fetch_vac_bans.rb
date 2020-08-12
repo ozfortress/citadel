b5085bd525f38dda66223dfe98bd385fc964dc5b
coretf <connororeilly@outlook.com.au>
2020-08-12 16:51:25 -0700

    Add medal_dump and fetch_vac_bans scripts from ozfortress branch

---
+++ script/fetch_vac_bans.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'json'
+
+# Checks all users in citadel for VAC bans
+# Distinguishing between games is impossible through the API,
+# so checks will have to be done manually.
+
+API_KEY = Rails.application.secrets.steam_api_key
+
+def get_bans(steam_ids)
+  ids = steam_ids.join(',')
+  url = "http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=#{API_KEY}&steamids=#{ids}"
+  response = Net::HTTP.get(URI(url))
+  json = JSON.parse(response)
+
+  json['players'].select { |player| player['VACBanned'] }
+                 .map { |player| player['SteamId'] }
+                 .map(&:to_i)
+end
+
+User.order(:id).find_in_batches(batch_size: 10) do |users|
+  steam_ids = users.map(&:steam_id)
+  bans = get_bans(steam_ids)
+  bans.each do |steam_id|
+    user = User.find_by(steam_id: steam_id)
+    puts("#{user.name}: https://warzone.ozfortress.com/users/#{user.id}")
+  end
+end
---
+++ script/medals_dump.rb
@@ -0,0 +1,28 @@
+# List of steam ids to exclude from medals
+VAC = [].freeze
+
+league = League.find(ARGV.first.to_i)
+
+def dump_roster(roster)
+  roster.users.find_each do |user|
+    puts user.steam_id unless VAC.include?(user.steam_id)
+  end
+end
+
+league.divisions.each do |division|
+  rosters = division.rosters.ordered.active
+
+  raise if rosters.first.ranking.nil?
+  dump_roster rosters.first
+  puts
+
+  raise if rosters.second.ranking.nil?
+  dump_roster rosters.second
+  puts
+
+  raise if rosters.third.ranking.nil?
+  dump_roster rosters.third
+  puts
+  rosters[3..rosters.length].each { |roster| dump_roster roster }
+  puts
+end
