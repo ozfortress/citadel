5621d278bc9ada555e766f1369dea226869de190
Benjamin Schaaf <ben.schaaf@gmail.com>
2020-08-20 22:04:02 +1000

    Add rails middleware to work around shitty rack bug
    For some reason this bug still hasn't been fixed: https://github.com/rack/rack/issues/903

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Required for some fucking reason
-require 'uri'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# Include middleware
+require './lib/middleware/bad_multipart_form_data_sanitizer'
 
 module Ozfortress
   class Application < Rails::Application
@@ -27,5 +27,8 @@ module Ozfortress
 
     # News config file
     config.news = config_for(:news)
+
+    # config.middleware.use 'BadMultipartFormDataSanitizer'
+    config.middleware.insert_before Rack::Runtime, BadMultipartFormDataSanitizer
   end
 end
