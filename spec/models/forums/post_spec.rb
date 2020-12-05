a6da795c6f8ad4d17be1903d7ba498efb9e2f130
Benjamin Schaaf <ben.schaaf@gmail.com>
2020-12-05 21:40:04 +1100

    Fix tests

--- spec/models/forums/post_spec.rb
+++ spec/models/forums/post_spec.rb
@@ -8,5 +8,5 @@ describe Forums::Post do
   it { should belong_to(:created_by).class_name('User') }
 
   it { should validate_presence_of(:content) }
-  it { should validate_length_of(:content).is_at_least(10).is_at_most(10_000) }
+  it { should validate_length_of(:content).is_at_least(10).is_at_most(50_000) }
 end
